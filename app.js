a35cd2899756161fd07651c61de9a12a7a7c4f00
ortizk <ortizk@live.com>
2018-04-05 15:50:52 -0700

    can check if an answer is correct

--- app.js
+++ app.js
@@ -2,37 +2,68 @@ console.log('JS is working!');
 
 $("#startGame").click( (e)=>{
 	console.log("start game button was clicked");
-	console.log(questions[0].q)
 	$("#questionForm").append("<p>" + questions[0].q + "</p>");
 	// will have to append a random question
 
 	// make this into a function
 	var opt1 = document.createElement("input");
 	opt1.type = "radio";
-	opt1.name = "options"
+	opt1.name = "options";
+	opt1.id = "opt1";
 	$("#questionForm").append(opt1, questions[0].opt1);
 
 	var opt2 = document.createElement("input");
 	opt2.type = "radio";
-	opt2.name = "options"
+	opt2.name = "options";
+	opt2.id = "opt2"
 	$("#questionForm").append(opt2, questions[0].opt2);
 
 	var opt3 = document.createElement("input");
 	opt3.type = "radio";
-	opt3.name = "options"
+	opt3.name = "options";
+	opt3.id = "opt3";
 	$("#questionForm").append(opt3, questions[0].opt3);
 
 	var opt4 = document.createElement("input");
 	opt4.type = "radio";
-	opt4.name = "options"
+	opt4.name = "options";
+	opt4.id = "opt4";
 	$("#questionForm").append(opt4, questions[0].opt4);
 
 	var submitButton = document.createElement("button");
+	submitButton.id = "checkAnswerButton"
 	textOnButton = document.createTextNode("check you answer")
 	submitButton.append(textOnButton)
 	$("#currentQuestion").append(submitButton)
 
+	$("#checkAnswerButton").click( (e) => {
+		console.log("check answer was clicked");
+		whichBox()
+	})
+
+	const whichBox = () => {
+		if($("#opt1")[0].checked && questions[0].opt1 === questions[0].answer) {
+			console.log("you got the right answer");
+		}
+		else if($("#opt2")[0].checked && questions[0].opt2 === questions[0].answer){
+			questions[0].opt1 === questions[0].answer
+			console.log("you got the right answer");
+		}
+		else if($("#opt3")[0].checked && questions[0].opt3 === questions[0].answer){
+			questions[0].opt1 === questions[0].answer
+			console.log("you got the right answer");
+		}
+		else if($("#opt4")[0].checked && questions[0].opt4 === questions[0].answer){
+			questions[0].opt1 === questions[0].answer
+			console.log("you got the right answer");
+		}
+
+	}
+
+
 })
+
+
 
 
 
--- questions.js
+++ questions.js
@@ -4,14 +4,16 @@ const questions = [
 		opt1: 2014,
 		opt2: 2008,
 		opt3: 2006,
-		opt4: 2000 
+		opt4: 2000,
+		answer: 2014 
 	},
 	{
 		q:"Jackson Polluck was an inlfuencial abstract expressionist painter from what country?",
 		opt1: "Brazil",
 		opt2: "France",
 		opt3: "Italy",
-		opt4: "United States"	
+		opt4: "United States",
+		answer: "United States"	
 	}
 	];
 
