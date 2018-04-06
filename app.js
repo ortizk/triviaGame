b73a81d054e9760e91486bde424ed42b9760238d
ortizk <ortizk@live.com>
2018-04-06 09:47:30 -0700

    can randomly chooses questions on reload and can check if they are right or wrong

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ console.log('JS is working!');
 
 $("#startGame").click( (e)=>{
 	console.log("start game button was clicked");
-	$("#questionForm").append("<p>" + questions[0].q + "</p>");
+	$("#questionForm").append("<p>" + questions[random].q + "</p>");
 	// will have to append a random question
 
 	// make this into a function
@@ -10,25 +10,25 @@ $("#startGame").click( (e)=>{
 	opt1.type = "radio";
 	opt1.name = "options";
 	opt1.id = "opt1";
-	$("#questionForm").append(opt1, questions[0].opt1);
+	$("#questionForm").append(opt1, questions[random].opt1);
 
 	var opt2 = document.createElement("input");
 	opt2.type = "radio";
 	opt2.name = "options";
 	opt2.id = "opt2"
-	$("#questionForm").append(opt2, questions[0].opt2);
+	$("#questionForm").append(opt2, questions[random].opt2);
 
 	var opt3 = document.createElement("input");
 	opt3.type = "radio";
 	opt3.name = "options";
 	opt3.id = "opt3";
-	$("#questionForm").append(opt3, questions[0].opt3);
+	$("#questionForm").append(opt3, questions[random].opt3);
 
 	var opt4 = document.createElement("input");
 	opt4.type = "radio";
 	opt4.name = "options";
 	opt4.id = "opt4";
-	$("#questionForm").append(opt4, questions[0].opt4);
+	$("#questionForm").append(opt4, questions[random].opt4);
 
 	var submitButton = document.createElement("button");
 	submitButton.id = "checkAnswerButton"
@@ -38,8 +38,8 @@ $("#startGame").click( (e)=>{
 
 	$("#checkAnswerButton").click( (e) => {
 		console.log("check answer was clicked");
+
 		checkAnswer();
-		// whichBox()
 	})
 
 });
@@ -47,75 +47,26 @@ $("#startGame").click( (e)=>{
 
 const checkAnswer = () => {
 	var option = $('input[name=options]:checked')[0].id;
-	if ($('input[name=options]:checked').length > 0 && questions[0][option] === questions[0].answer ) {
+	if ($('input[name=options]:checked').length > 0 && questions[random][option] === questions[random].answer ) {
 		console.log($('input[name=options]:checked')[0].id)
 		console.log("RIGHT");
-		console.log('correct answer', questions[0].answer);
-		console.log('picked answer', questions[0][option]);
+		console.log('correct answer', questions[random].answer);
+		console.log('picked answer', questions[random][option]);
 	}
 	else {
 		console.log("WRONG");
 	}
 }
 
-// 	const whichBox = () => {
-// 		if($("#opt1")[0].checked && questions[0].opt1 === questions[0].answer) {
-// 			console.log("you got the right answer");
-// 		}
-// 		else if($("#opt2")[0].checked && questions[0].opt2 === questions[0].answer){
-// 			questions[0].opt1 === questions[0].answer
-// 			console.log("you got the right answer");
-// 		}
-// 		else if($("#opt3")[0].checked && questions[0].opt3 === questions[0].answer){
-// 			questions[0].opt1 === questions[0].answer
-// 			console.log("you got the right answer");
-// 		}
-// 		else if($("#opt4")[0].checked && questions[0].opt4 === questions[0].answer){
-// 			questions[0].opt1 === questions[0].answer
-// 			console.log("you got the right answer");
-// 		}
+// trying to make it so the player can't change their selection once answer is submitted
+// $(":radio").click(function() {
+// 	var options = $(this).attr("name");
+// 	$(":radio[name='"+options+"']:not(:checked)").attr("disabled", true);
+// });
 
-// 	}
+var random = Math.floor((Math.random() * 10) + 1);
+console.log(random);
 
-
-// })
-// ---------------------------------------trying to write code to check if it's right AND wrong
-
-// const whichBox = () => {
-// 		if($("#opt1")[0].checked && questions[0].opt1 === questions[0].answer) {
-// 			console.log("you got the right answer");
-// 		}
-// 		else if($("#opt2")[0].checked && questions[0].opt2 === questions[0].answer){
-// 			questions[0].opt1 === questions[0].answer
-// 			console.log("you got the right answer");
-// 		}
-// 		else if($("#opt3")[0].checked && questions[0].opt3 === questions[0].answer){
-// 			questions[0].opt1 === questions[0].answer
-// 			console.log("you got the right answer");
-// 		}
-// 		else if($("#opt4")[0].checked && questions[0].opt4 === questions[0].answer){
-// 			questions[0].opt1 === questions[0].answer
-// 			console.log("you got the right answer");
-// 		}
-
-// 	}
-
-// const checkFirstBox = () => {
-// 	if($("#opt1")[0].checked && questions[0].opt1 === questions[0].answer) {
-// 			console.log("RIGHT");
-// 		}
-// 	else {
-// 		console.log("WRONG")
-// 	}
-// }
-// const checkAnswer = () => {
-// 	if ($('input[name=options]:checked').length > 0 && questions[0].opt1 === questions[0].answer ) {
-// 		console.log("RIGHT");
-// 	}
-// 	else {
-// 		console.log("WRONG");
-// 	}
-// }
 
 
 
--- questions.js
+++ questions.js
@@ -8,13 +8,77 @@ const questions = [
 		answer: 2014 
 	},
 	{
-		q:"Jackson Polluck was an inlfuencial abstract expressionist painter from what country?",
+		q:"Jackson Polluck was an inlfuencial abstract expressionist painter from what country",
 		opt1: "Brazil",
 		opt2: "France",
 		opt3: "Italy",
 		opt4: "United States",
 		answer: "United States"	
-	}
+	},
+	{
+		q:"What city connects two continents",
+		opt1: "Minsk",
+		opt2: "San Diego",
+		opt3: "Istanbul",
+		opt4: "Ulaanbaatar",
+		answer: "Istanbul" 
+	},
+	{
+		q:"At what temperature are Fahrenheit and Celsius equal",
+		opt1: 20,
+		opt2: -40,
+		opt3: 100,
+		opt4: 2000,
+		answer: -40 
+	},
+	{
+		q:"What is the maximum amount of Michelin stars a restaurant can recieve",
+		opt1: 8,
+		opt2: 10,
+		opt3: 5,
+		opt4: 3,
+		answer: 3 
+	},
+	{
+		q:"An octopus can fit through any whole larger than its _________",
+		opt1: "Beak",
+		opt2: "Head",
+		opt3: "Tenticles",
+		opt4: "Brain",
+		answer: "Beak" 
+	},
+	{
+		q:"What was the first console video game that allowed the game to be saved",
+		opt1: "Super Mario Bros.",
+		opt2: "Ms. Pac-Man",
+		opt3: "Duck Hunter",
+		opt4: "The Legend Of Zelda",
+		answer: "The Legend Of Zelda" 
+	},
+	{
+		q:"Bobby Fischer is considered by many to be the greatest player of all time in which game",
+		opt1: "Chinese Checkers",
+		opt2: "Basketball",
+		opt3: "Chess",
+		opt4: "Cricket",
+		answer: "Chess" 
+	},
+	{
+		q:"What is the second largest country by land mass",
+		opt1: "Brazil",
+		opt2: "Canada",
+		opt3: "Russia",
+		opt4: "Australia",
+		answer: "Canada" 
+	},
+	{
+		q:"Canada is made up of how many provinces",
+		opt1: 10,
+		opt2: 20,
+		opt3: 36,
+		opt4: 8,
+		answer: 10 
+	},
 	];
 
 
